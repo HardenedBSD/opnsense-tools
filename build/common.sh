3fed343344f7ae62f717a164c8a296c9b2359abc
Shawn Webb <shawn.webb@hardenedbsd.org>
2015-05-11 00:10:59 -0400

    Separate out the config variables to its own config file

--- build/common.sh
+++ build/common.sh
@@ -27,43 +27,22 @@
 
 set -e
 
-# important build settings
-export PRODUCT_VERSION=${PRODUCT_VERSION:-$(date '+%Y%m%d%H%M')}
-export PRODUCT_FLAVOUR=${PRODUCT_FLAVOUR:-"OpenSSL"}
-export PRODUCT_NAME="OPNsense"
+configfile=""
 
-# full name for easy use
-export PRODUCT_RELEASE="${PRODUCT_NAME}-${PRODUCT_VERSION}_${PRODUCT_FLAVOUR}"
+while getopts "c:" o; do
+	case "${o}" in
+	c)
+		configfile="${OPTARG}"
+		;;
+	esac
+done
 
-# code reositories
-export TOOLSDIR="/usr/tools"
-export PORTSDIR="/usr/ports"
-export COREDIR="/usr/core"
-export SRCDIR="/usr/src"
+if [ "${configfile}" = "" ] || [ ! -f "${configfile}" ]; then
+	echo "Configuration file required (specify with -c)"
+	exit 1
+fi
 
-# misc. foo
-export CONFIG_PKG="/usr/local/etc/pkg/repos/${PRODUCT_NAME}.conf"
-export CPUS=$(sysctl kern.smp.cpus | awk '{ print $2 }')
-export CONFIG_XML="/usr/local/etc/config.xml"
-export ARCH=${ARCH:-$(uname -m)}
-export LABEL=${PRODUCT_NAME}
-export TARGET_ARCH=${ARCH}
-export TARGETARCH=${ARCH}
-
-# define target directories
-export PACKAGESDIR="/packages"
-export STAGEDIR="/usr/local/stage"
-export IMAGESDIR="/tmp/images"
-export SETSDIR="/tmp/sets"
-
-# bootstrap target directories
-mkdir -p ${STAGEDIR} ${IMAGESDIR} ${SETSDIR}
-
-# target files
-export CDROM="${IMAGESDIR}/${PRODUCT_RELEASE}-cdrom-${ARCH}.iso"
-export SERIALIMG="${IMAGESDIR}/${PRODUCT_RELEASE}-serial-${ARCH}.img"
-export VGAIMG="${IMAGESDIR}/${PRODUCT_RELEASE}-vga-${ARCH}.img"
-export NANOIMG="${IMAGESDIR}/${PRODUCT_RELEASE}-nano-${ARCH}.img"
+. "${configfile}"
 
 # print environment to showcase all of our variables
 env
